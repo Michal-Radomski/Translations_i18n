8483d3d7846139d6b9fc262f13a84a67b71ed43e
Michal Radomski <michalra@op.pl>
2021-10-01 22:40:46 +0200

    React translate done

--- 02 React Translate/src/App.tsx
+++ 02 React Translate/src/App.tsx
@@ -1,16 +1,44 @@
 import React from "react";
 import axios from "axios";
-import "./App.css";
 
+import i18next from "i18next";
+
+type State = {
+  tekst: string;
+};
 class App extends React.Component {
-  state = {tekst: ""};
+  state: State = {tekst: ""};
+  lang: string = "en";
 
   componentDidMount = async () => {
+    await i18next.changeLanguage(this.lang);
+    const config: string | any = axios.defaults.headers;
+    config["Accept-Language"] = this.lang;
+    // console.log(config["Accept-Language"]);
     const response = await axios.get("http://localhost:8000/text");
     const temp1 = response.data;
     const temp2 = temp1["text"];
-    console.log(temp2);
+    // console.log(temp2);
     this.setState({tekst: temp2});
+    // console.log(this.state);
+  };
+
+  change = async (lang: string) => {
+    this.lang = lang;
+    await this.componentDidMount();
+  };
+
+  click = async () => {
+    const response = await axios.post("http://localhost:8000/like");
+    // console.log(response);                      // -Odpowiedź z serwera
+    const temp1 = response.data["message"];
+    if (!temp1) {
+      console.log("waiting");
+    }
+    // console.log(temp1);
+    const text: string = i18next.t("thank_you");
+    // console.log(text);                          // -Odpowiedź z aplikacji
+    alert(text);
   };
 
   render() {
@@ -19,7 +47,7 @@ class App extends React.Component {
         <div className="row py-3">
           <div className="col-10"></div>
           <div className="col-2">
-            <select className="form-control">
+            <select className="form-control" onChange={(e) => this.change(e.target.value)}>
               <option value="en">English</option>
               <option value="pl">Polish</option>
             </select>
@@ -27,9 +55,13 @@ class App extends React.Component {
         </div>
         <div className="row">
           <div className="col-md12">
-            <h2>This text is translated in the frontend</h2>
+            {/* <h2>This text is translated in the frontend:</h2> */}
+            <h2>{i18next.t("title")}</h2>
+            {/* <p>This is translated in the backend:</p> */}
             <p>{this.state.tekst}</p>
-            <button className="btn btn-outline-primary">Like</button>
+            <button className="btn btn-outline-primary" onClick={this.click}>
+              {i18next.t("like")}
+            </button>
           </div>
         </div>
       </main>
