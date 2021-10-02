91404330c505262a1e2254bb44646d33e3d484c9
Michal Radomski <michalra@op.pl>
2021-10-02 02:36:51 +0200

    Angular - commit2

--- 03 Angular Translate/src/app/app.component.ts
+++ 03 Angular Translate/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-translations';
+  // title = 'angular-translations';
+  text = '';
+  lang = 'en';
+  constructor(private http: HttpClient) {
+    this.load();
+  }
+
+  load(): void {
+    const headers = new HttpHeaders({
+      'Accept-Language': this.lang,
+    });
+
+    this.http
+      .get('http://localhost:8000/text', { headers: headers })
+      .subscribe((res: any) => (this.text = res.text));
+  }
+  change(lang: any): void {
+    this.lang = lang;
+    this.load();
+  }
 }
