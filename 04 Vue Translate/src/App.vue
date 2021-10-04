4798785a2b6ed4f0b9976e68b4886a7f9765808b
Michal Radomski <michalra@op.pl>
2021-10-04 18:39:08 +0200

    Vue Translate done

--- 04 Vue Translate/src/App.vue
+++ 04 Vue Translate/src/App.vue
@@ -3,7 +3,7 @@
     <div class="row py-3">
       <div class="col-10"></div>
       <div class="col-2">
-        <select class="form-control">
+        <select class="form-control" @change="change($event.target.value)">
           <option value="en">English</option>
           <option value="pl">Polish</option>
         </select>
@@ -11,9 +11,9 @@
     </div>
     <div class="row">
       <div class="col-md12">
-        <h2>This text is translated in the frontend:</h2>
-        <p>This is translated in the backend:</p>
-        <button class="btn btn-outline-primary">Like</button>
+        <h2>{{ t("title", {}, {locale: lang}) }}</h2>
+        <p>{{ text }}</p>
+        <button class="btn btn-outline-primary" @click="click()">{{ t("like", {}, {locale: lang}) }}</button>
       </div>
     </div>
   </main>
@@ -21,8 +21,48 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import axios from "axios";
+import {useI18n} from "vue-i18n";
 
 export default defineComponent({
   name: "App",
+  setup() {
+    const {t, locale} = useI18n();
+    return {t, locale};
+  },
+  data() {
+    return {
+      text: "",
+      lang: "en",
+    };
+  },
+  async mounted() {
+    await this.load();
+  },
+  methods: {
+    async load() {
+      // axios.defaults.headers["Accept-Language"] = this.lang;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const config: string | any = axios.defaults.headers;
+      config["Accept-Language"] = this.lang;
+      const response = await axios.get("http://localhost:8000/text");
+      this.text = response.data["text"];
+    },
+
+    async change(lang: string) {
+      this.lang = lang;
+      this.load();
+    },
+    async click() {
+      const response = await axios.post("http://localhost:8000/like");
+      const temp = response.data["message"];
+      if (!temp) {
+        console.log("waiting");
+      }
+
+      const text: string = this.t("thank_you", {}, {locale: this.lang});
+      alert(text);
+    },
+  },
 });
 </script>
--- 04 Vue Translate/src/main.ts
+++ 04 Vue Translate/src/main.ts
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import en from "./translations/en.json";
+import pl from "./translations/pl.json";
+import {createI18n} from "vue-i18n";
+
+const i18n = createI18n({
+  messages: {
+    en: en,
+    pl: pl,
+  },
+  fallbackLocale: "en",
+});
+
+const app = createApp(App);
+app.use(i18n);
+app.mount("#app");
