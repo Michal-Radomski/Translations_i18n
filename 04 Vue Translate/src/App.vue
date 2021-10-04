1b055f45923638403425b578444b04c0c3043582
Michal Radomski <michalra@op.pl>
2021-10-04 15:42:56 +0200

    vue - commit1

--- 04 Vue Translate/src/App.vue
+++ 04 Vue Translate/src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="container">
+    <div class="row py-3">
+      <div class="col-10"></div>
+      <div class="col-2">
+        <select class="form-control">
+          <option value="en">English</option>
+          <option value="pl">Polish</option>
+        </select>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md12">
+        <h2>This text is translated in the frontend:</h2>
+        <p>This is translated in the backend:</p>
+        <button class="btn btn-outline-primary">Like</button>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {defineComponent} from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
